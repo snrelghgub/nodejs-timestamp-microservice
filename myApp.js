f948be062a0b0008e40b218f925c68e7f7ea2a7f
snrelghgub <snrelghmail@gmail.com>
2019-08-30 16:03:42 +1000

    added listening port, index.html & static file style.css

--- myApp.js
+++ myApp.js
@@ -1,1 +1,18 @@
-console.log ("Hello World!"); 
+
+
+const express = require('express'); 
+const app = express(); 
+
+
+//serve static files, e.g. style.css 
+app.use(express.static(__dirname + 'public'));
+
+//display index page index.html @ "/"
+app.get("/", function(req, res){
+ res.sendFile(__dirname + '/views/index.html'); 
+}); 
+
+//listening port => localhost:3000/
+app.listen("3000", function(req,res){
+  console.log("listening on port 3000"); 
+});
